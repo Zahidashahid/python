ba3f5febfb74ec9c93613a00ebbfe44d19fcaa62
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2021-07-31 15:58:21 +0500

    defalut perameters

--- src/Functions/defultPerameter_functions.py
+++ src/Functions/defultPerameter_functions.py
@@ -1,4 +1,10 @@
 def loginUser(name="admin", password="admin"):
+    """
+    default arguments will use when user not send name and password
+    :param name:
+    :param password:
+    :return:
+    """
     print("Name is {}  password is {} ".format(name, password))
 
 
