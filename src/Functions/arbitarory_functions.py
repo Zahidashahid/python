459f5a78e69df5819ec584aee5998f0aebafa208
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2021-07-31 18:29:02 +0500

    all functions typed used in single fuction

--- src/Functions/arbitarory_functions.py
+++ src/Functions/arbitarory_functions.py
@@ -1,24 +1,30 @@
+def addition_function(*values):
+    result = 0
+    for i in values:
+        result += i
+    print("Sum is:", result)
 
 
-def advance_junaid(name, roll, *marks, area="GB", school="GBPS"):
+def advance_junaid(name, roll, *marks, area="GB", school="GBPS", **keywordArg):
     """
-     here marks is arbitrary
+     Here marks is arbitrary
     :param name:
     :param roll:
     :param marks:
     :param area:
     :param school:
+    :param keywordArg:
     :return:
     """
     total_subjects = len(marks)
-    total_marks = len(marks)*100
+    total_marks = len(marks) * 100
     gained_marks = 0
     is_passed = False
 
     for subject_marks in marks:
         gained_marks += subject_marks
 
-    if gained_marks >= total_marks/2:
+    if gained_marks >= total_marks / 2:
         is_passed = True
 
     print("area  : ", area)
@@ -34,3 +40,9 @@ def advance_junaid(name, roll, *marks, area="GB", school="GBPS"):
 
     print("---------------------")
 
+    for values, keyword in keywordArg.items():
+        print(values, keyword)
+
+
+addition_function(1, 2)
+advance_junaid("zahida", 166, 25, 25, contact=1031256589, language="Urdu", area="Danyore", school="DJ")
--- src/Functions/keywordArbitraryArguments.py
+++ src/Functions/keywordArbitraryArguments.py
@@ -6,3 +6,13 @@ def person(name, **data):
 
 
 person("zahida", roll=166, contact=1234567, country="Pakistan")
+
+
+def student(name,*contact , **marks ):
+    print(name)
+    print(contact)
+    for i, j in marks.items():
+        print(i, j)
+
+
+student("zahida", 123, 415, maths=1, english=2, urdu=3 )
