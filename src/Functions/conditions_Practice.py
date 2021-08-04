b8ea0b310e07490fddf6762d182b9fb9c42bde01
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2021-08-04 22:36:42 +0500

    Loops and conditions

---
+++ src/Functions/conditions_Practice.py
@@ -0,0 +1,60 @@
+"""
+package : practice
+chapter : Conditional control statements
+topics  : [IF, ELSE, ELIF, no witch cases in python]
+if      : if_keyword condition : elif condition : else :
+"""
+
+name = input("Enter name: ")
+
+# IF ONLY
+if name == 'wakas':
+    print("Name is equal")
+
+# IF OR ELSE
+if name == 'wakas':
+    print("Name is equal")
+else:
+    print("Not Equal")
+
+# IF OR ELSE
+if name == 'wakas':
+    print("Name is equal")
+else:
+    print("Not Equal")
+
+# IF ELIF or ELSE
+if name == 'wakas':
+    print("Wakas")
+elif name == 'ikram':
+    print("Ikram")
+elif name == 'junaid':
+    print("junaid")
+else:
+    print("Not -available")
+
+# why to use conditional control
+
+
+# Types and uses
+
+
+# IF
+
+
+# IF-ELSE
+
+
+# IF_ELSE-IF / IF_ELIF
+
+
+# Ternary Operators or Conditional Expressions
+
+
+# Nested IF
+
+
+# PASS
+
+
+# AND / OR / >  / >= / < / <= / == / != / is / is not / in / not in / bool()
